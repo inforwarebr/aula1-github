d84371cd96fb4afbaf64a6ba2e9f2d5ebd8b0a90
Celso Pires <celsospires@gmail.com>
2019-01-05 07:57:18 -0300

    Projeto criado

---
+++ src/course/Program.java
@@ -0,0 +1,28 @@
+package course;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import java.util.Scanner;
+
+import entities.Account;
+import entities.Produto;
+import entities.Quartos;
+import entities.Triangulo;
+
+public class Program {
+
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+  
+        SimpleDateFormat sdf1 = new SimpleDateFormat("dd/MM/yyyy");
+        
+        
+        sc.close();
+	}
+
+}
